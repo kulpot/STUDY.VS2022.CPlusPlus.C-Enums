c668f36cc51880aef91e4ad0eab47a238e0a66b3
Kulpot <kulpotwahu@gmail.com>
2023-12-16 17:39:14 +0800

    ...

--- C++ Enums/CppScratchPad.cpp
+++ C++ Enums/CppScratchPad.cpp
@@ -6,7 +6,49 @@
 using std::cout;
 using std::endl;
 
+// case: track a certain animal
+
+//const int SHEEP = 0;
+//const int COW = 1;
+//const int DONKEY = 2;
+//const int FISH = 3;
+
+enum Animals
+{
+	SHEEP,
+	COW,
+	DONKEY,
+	FISH,
+	MAX
+};
+
 void main()
 {
+	for (int i = 0; i < Animals::MAX; i++)
+	{
 
+	}
+
+	Animals a = Animals::FISH;
+	cout << COW << endl;
+	cout << a << endl;
+	int i = a;
+	cout << i << endl;
+	Animals b = static_cast<Animals>(i);
+	cout << b << endl;
+
+	int myAnimal = DONKEY;
+	// ...
+	if (myAnimal == FISH)
+	{
+
+	}
+	else if(myAnimal == DONKEY)
+	{
+
+	}
+	else
+	{
+
+	}
 }
