20d1933993169b1724a58edc25ebdfa3220fb5c5
Kulpot <kulpotwahu@gmail.com>
2023-12-16 17:29:00 +0800

    Add project files.

---
+++ C++ Enums/CppScratchPad.cpp
@@ -0,0 +1,12 @@
+// ------------------------------ C++ Enums --------------------------------
+// ref link:https://www.youtube.com/watch?v=b_OpPRWG-T8&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=7&t=3s
+
+#include <iostream>
+#include <cstdlib>
+using std::cout;
+using std::endl;
+
+void main()
+{
+
+}
